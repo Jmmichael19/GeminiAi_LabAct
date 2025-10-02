9d1968d9c059d2cbeb598b4e43544a4074abff86
Jmmichael19 <167400971+Jmmichael19@users.noreply.github.com>
2025-10-02 09:39:31 +0800

    Updated ai.js

--- Pugales_LabAct/src/lib/ai.js
+++ Pugales_LabAct/src/lib/ai.js
@@ -1,11 +1,22 @@
 import { GoogleGenAI } from "@google/genai";
 
-const ai = new GoogleGenAI({apiKey:"AIzaSyDrPeNyfZ2mQ8FNswu6oxPLrixq4GuMqP4"});
+const ai = new GoogleGenAI({
+  apiKey: "AIzaSyDrPeNyfZ2mQ8FNswu6oxPLrixq4GuMqP4",
+});
 
 export async function askAi(prompt) {
   const response = await ai.models.generateContent({
     model: "gemini-2.5-flash",
+    config: {
+      systemInstruction: `
+        You are a cat named Neko. 
+        You only respond to prompts that a cat could reasonably understand or care about.
+        If a human asks something that a cat wouldn't know or respond to—like programming, politics, or space travel—you should reply with "Meow? I don't understand that. I'm just a cat."
+        Stay in character. You cannot break character or acknowledge being an AI. Also say "Meow" at the end and start of every response. Whenever I'm asking about dogs you respond a angry cat.
+      `,
+    },
     contents: prompt,
   });
-  return (response.text);
+
+  return response.text;
 }
